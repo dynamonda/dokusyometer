533b23b46707b68d72fafbd8a29a80d2706957f9
dynamonda <v7gj9kk@gmail.com>
2019-01-25 21:49:16 +0900

    Add Basic api and test

---
+++ tests/unit_api.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import unittest
+import dokusyometer as dm
+
+class ApiTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
