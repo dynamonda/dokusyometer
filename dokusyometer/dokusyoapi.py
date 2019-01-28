9828b1960402a431dc819c1c5d0306a93ec5691f
dynamonda <v7gj9kk@gmail.com>
2019-01-28 15:54:53 +0900

    Add read_books_from_user_id

--- dokusyometer/dokusyoapi.py
+++ dokusyometer/dokusyoapi.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import math
+import re
 from bs4 import BeautifulSoup
 import requests
 import requests_cache
@@ -22,6 +24,38 @@ def user_datas_from_id(id):
         item = soup.find("div", class_="userdata-side__name")
         datas["name"] = item.string
     return datas
+
+def read_books_from_user_id(id):
+    if isinstance(id, int) == False:
+        raise TypeError
+
+    requests_cache.install_cache()
+    base_url = 'https://bookmeter.com/users/'+str(id)+'/books/read?page='
+    response = requests.get(base_url + str(1), headers=HEADERS)
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    books_num = int(soup.find('div', class_='content__count').string)
+    page_num = math.ceil(books_num / 20.0)
+
+    books = list()
+    for i in range(1, page_num+1):
+        response = requests.get(base_url + str(i), headers=HEADERS)
+        soup = BeautifulSoup(response.text, 'lxml')
+        book_groups = soup.find_all('ul', class_='book-list__group')
+        for book_group in book_groups:
+            for book in book_group.children:
+                bookdic = {}
+                title = book.find('div', class_='detail__title')
+                bookdic['title'] = title.a.string
+                bookdic['id'] = re.search(r'([0-9]+)', title.a['href']).group(0)
+                registed_date = book.find('div', class_="detail__date").string
+                bookdic['registed_date'] = registed_date
+                href_review = book.find('a', class_="icon__review")
+                if href_review is not None:
+                    href_review_id = href_review['href']
+                    bookdic['review_id'] = re.search(r'([0-9]+)', href_review_id)
+                books.append(bookdic)
+    return books
 
 def main():
     pass
--- test/test_unit_api.py
+++ test/test_unit_api.py
@@ -10,18 +10,28 @@ class ApiTestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_user_datas_from_id(self):
+    def test_user_datas_from_id_name(self):
         datas = dapi.user_datas_from_id(199863)
         self.assertEqual(datas['name'], 'dynamonda')
 
         datas = dapi.user_datas_from_id(452601)
         self.assertEqual(datas['name'], 'さっとる◎')
-        
+
         with self.assertRaises(TypeError):
             dapi.user_datas_from_id("string")
 
         datas = dapi.user_datas_from_id(9999999999)
         self.assertEqual(datas['name'], 'NotFound')
+
+    def test_read_books_from_user_id(self):
+        datas = dapi.read_books_from_user_id(199863)
+        self.assertEqual(len(datas), 409)
+
+        datas = dapi.read_books_from_user_id(567419)
+        self.assertEqual(len(datas), 159)
+
+        with self.assertRaises(TypeError):
+            dapi.read_books_from_user_id("string")
 
 if __name__ == '__main__':
     unittest.main()
